94125d7c36f18f98dbf6fa3d100b2dcf26c177fc
Chun Ho Chow <15370962+chunhochow@users.noreply.github.com>
2023-06-13 16:53:11 -0700

    champ_network: geocode by direction and misc
    - geocode/filter by direction
    - rename (inc inside function) find_edges_from_streetnames to find_paths_from_streetnames
    - remove igraph dependency:
      - node indices in champ_digraph now are their actual CHAMP indices

--- champ_network.py
+++ champ_network.py
@@ -5,20 +5,20 @@ import geopandas as gpd
 import networkx as nx
 import numpy as np
 import pandas as pd
-from igraph import Graph
+
+
+def read_champ_nodes(champ_nodes_gis_filepath) -> gpd.GeoDataFrame:
+    return gpd.read_file(champ_nodes_gis_filepath).set_index("N")
 
 
 def load_champ_network(
-    champ_links_gis_filepath: str, champ_nodes_gis_filepath: str
+    champ_links_gis_filepath: str, champ_nodes_gdf: gpd.GeoDataFrame
 ) -> nx.DiGraph:
-    champ_links = gpd.read_file(champ_links_gis_filepath)
-    champ_nodes = gpd.read_file(champ_nodes_gis_filepath)
-    # allows loading of edges and vertices in one function
-    # TODO if igraph is not needed elsewhere, load using networkx
-    g = Graph.DataFrame(  # igraph Graph
-        champ_links, directed=True, vertices=champ_nodes, use_vids=False
+    champ_links_gdf = gpd.read_file(champ_links_gis_filepath)
+    DG = nx.from_pandas_edgelist(
+        champ_links_gdf, "A", "B", edge_attr=True, create_using=nx.DiGraph
     )
-    DG = g.to_networkx()  # networkx DiGraph
+    nx.set_node_attributes(DG, champ_nodes_gdf.to_dict(orient="index"))
     return DG
 
 
@@ -152,7 +152,7 @@ def is_match_edge_name(
     return is_match_street_names(edge_name, edge_type, name, type, fuzzy=fuzzy)
 
 
-def find_edges_from_streetnames(
+def find_paths_from_streetnames(
     champ_digraph: nx.DiGraph,
     primary_street_name: str,
     primary_street_type: str,
@@ -165,17 +165,17 @@ def find_edges_from_streetnames(
 
 
     Logic flow (street name here refers to both street name and type):
-    0. edges_found = []
+    0. paths_found = []
     1. Search for all edges that matches the primary street name.
     2. For the two end-nodes of each matching edge, check all their in and out
        edges to see if their names matches the cross streets'.
     3. If both end-nodes of an edge intersects with the two cross streets, then
-       append that segment to edges_found.
+       append that segment to paths_found.
     4. If no edges satisfy the condition in 3, look up the list of intersections
        that intersect with each cross street. Do a shortest path (by actual
        distance) routing between them, and only keep this shortest path if each
        edge in the shortest path matches the primary street name. Add these to
-       edges_found.
+       paths_found.
 
     TODO Might want to do an exact (not fuzzy) match for street names first,
     and only do the fuzzy match (can even do this in stages) if there isn't
@@ -206,7 +206,7 @@ def find_edges_from_streetnames(
     Returns
     -------
     _type_
-        _description_
+        list of paths (sequence of nodes)
 
     Raises
     ------
@@ -215,7 +215,7 @@ def find_edges_from_streetnames(
     """
     cross1_nodes = []  # nodes where primary st and cross st 1 meet
     cross2_nodes = []  # nodes where primary st and cross st 2 meet
-    edges_found = []
+    paths_found = []
     # check each edge to see if its name & type match the primary street's
     for primary_st_node1, primary_st_node2 in champ_digraph.edges():
         # if the edge's name & type match the primary street's
@@ -275,20 +275,20 @@ def find_edges_from_streetnames(
             # enforce that the each end-node matches a different cross street
             if cross_street_1_found and cross_street_2_found:
                 # this particular street segment matches a CHAMP link
-                edges_found.append([primary_st_node1, primary_st_node2])
+                paths_found.append([primary_st_node1, primary_st_node2])
     # N.B. if all of the primary and cross streets are bidirectional,
     # len(cross1_nodes) == len(cross2_nodes) == 16
-    if edges_found:  # if not empty
-        # N.B. len(edges_found) == 2 if the street segment is bidirectional,
-        # otherwise len(edges_found) == 1 if it's one-way
-        return edges_found
-    # if edges_found is empty but cross1/2_nodes both not empty
+    if paths_found:  # if not empty
+        # N.B. len(paths_found) == 2 if the street segment is bidirectional,
+        # otherwise len(paths_found) == 1 if it's one-way
+        return paths_found
+    # if paths_found is empty but cross1/2_nodes both not empty
     elif cross1_nodes and cross2_nodes:
         # This particular street segment is not a CHAMP link,
         # but the nodes for the two intersections are found,
         # which means that the street segment spans multiple CHAMP links.
         # So we just do a shortest routing (weighted by distance of each edge)
-        # between the two nodes and return that as edges_found
+        # between the two nodes and return that as paths_found
         cross1_nodes = set(cross1_nodes)
         cross2_nodes = set(cross2_nodes)
         # TODO issue: this can give a ton of routing options if
@@ -311,7 +311,101 @@ def find_edges_from_streetnames(
                     )
                     for edge_nodes in pairwise(shortest_path)
                 ):
-                    edges_found.append(shortest_path)
-        return edges_found
+                    paths_found.append(shortest_path)
+        return paths_found
     else:
-        return []  # == edges_found
+        return []  # == paths_found
+
+
+def get_link_direction(
+    champ_nodes_gdf: gpd.GeoDataFrame,
+    node_i: int,
+    node_f: int,
+    direction_pair: str,
+):
+    """_summary_
+
+    Parameters
+    ----------
+    champ_nodes_gdf : gpd.GeoDataFrame
+        indexed by the node names
+        (i.e. node_i and node_f should refer champ_nodes_gdf's index)
+    node_i : int
+        _description_
+    node_f : int
+        _description_
+    direction_pair : str
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+
+    Raises
+    ------
+    RuntimeError
+        _description_
+    ValueError
+        _description_
+    """
+    if direction_pair == "NS":
+        node_i_y = champ_nodes_gdf.loc[node_i]["Y"]
+        node_f_y = champ_nodes_gdf.loc[node_f]["Y"]
+        if node_i_y < node_f_y:
+            return "NB"
+        elif node_i_y > node_f_y:
+            return "SB"
+        else:
+            raise RuntimeError("The two nodes have the same Y value.")
+    elif direction_pair == "EW":
+        node_i_x = champ_nodes_gdf.loc[node_i]["X"]
+        node_f_x = champ_nodes_gdf.loc[node_f]["X"]
+        if node_i_x < node_f_x:
+            return "EB"
+        elif node_i_x > node_f_x:
+            return "WB"
+        else:
+            raise RuntimeError("The two nodes have the same X value.")
+    else:
+        raise ValueError('direction_pair should be "NS" or  "EW".')
+
+
+def filter_by_direction(
+    champ_nodes_gdf: gpd.GeoDataFrame, path_list, direction: str
+):
+    """Get the path in path_list that corresponds to direction
+
+    Algorithm is based on comparing the X/Y value of the initial and final nodes
+
+    Parameters
+    ----------
+    champ_nodes_gdf : gpd.GeoDataFrame
+        the index should be the node name referenced in paths_list
+    path_list : _type_
+        list of paths (sequece of nodes)
+    direction : str
+        NB/SB/EB/WB
+    """
+    if direction in {"NB", "SB"}:
+        direction_edge_dict = {
+            get_link_direction(
+                champ_nodes_gdf, edge_list[0], edge_list[-1], "NS"
+            ): edge_list
+            for edge_list in path_list
+        }
+    elif direction in {"EB", "WB"}:
+        direction_edge_dict = {
+            get_link_direction(
+                champ_nodes_gdf, edge_list[0], edge_list[-1], "EW"
+            ): edge_list
+            for edge_list in path_list
+        }
+    else:
+        raise ValueError('direction should be "NB", "SB", "EB", or "WB".')
+    try:
+        return direction_edge_dict[direction]
+    except KeyError:
+        raise RuntimeError(
+            f"Direction {direction} does not exist in path_list."
+        )
