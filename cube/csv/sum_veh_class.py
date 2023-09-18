2222cb273ff7aef4625ca4174b75b5f581c39e83
Chun Ho Chow <15370962+chunhochow@users.noreply.github.com>
2023-09-18 14:46:25 -0700

    sum_veh_class.py: add argparse, debug polars changes to sum()

--- cube/csv/sum_veh_class.py
+++ cube/csv/sum_veh_class.py
@@ -1,19 +1,25 @@
+import argparse
 from pathlib import Path
 
 import polars as pl
 
-champ_periods = ["EA", "AM", "MD", "PM", "EV"]
-veh_class_cols = [f"V{i}_1" for i in range(1, 19)]
-dir = r"X:\Projects\CHAMP7\Run6_Third"
-for champ_period in champ_periods:
-    filename_root = f"LOAD{champ_period}_FINAL"
-    pl.scan_csv(
-        Path(dir, f"{filename_root}.csv"),
-        dtypes={col: pl.Float64 for col in veh_class_cols},
-    ).with_columns(
-        pl.sum([f"V{i}_1" for i in range(1, 19)]).alias("sum(V1_1..V18_1)")
-    ).select(
-        ["A", "B", "sum(V1_1..V18_1)"]
-    ).collect().write_csv(
-        Path(dir, f"{filename_root}-veh_class_sum.csv")
-    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    args = parser.parse_args()
+
+    champ_periods = ["EA", "AM", "MD", "PM", "EV"]
+    veh_class_cols = [f"V{i}_1" for i in range(1, 19)]
+    for champ_period in champ_periods:
+        filename_root = f"LOAD{champ_period}_FINAL"
+        df = pl.scan_csv(
+            Path(args.directory, f"{filename_root}.csv"),
+            dtypes={col: pl.Float64 for col in veh_class_cols},
+        ).with_columns(
+            pl.sum_horizontal(veh_class_cols).alias("sum(V1_1..V18_1)")
+        ).select(
+            ["A", "B", "sum(V1_1..V18_1)"]
+        ).collect().write_csv(
+            Path(args.directory, f"{filename_root}-veh_class_sum.csv")
+        )
